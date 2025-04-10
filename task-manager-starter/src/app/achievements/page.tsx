78ea912551487d25e68a6e496f2c6c4660b90e02
oneilKat <oneil.kat@northeastern.edu>
2025-04-10 19:23:21 -0400

    Made Sidebar a component

--- task-manager-starter/src/app/achievements/page.tsx
+++ task-manager-starter/src/app/achievements/page.tsx
@@ -3,26 +3,17 @@
 import { Progress } from "@heroui/react";
 import { Card, CardBody } from "@heroui/react";
 import { Avatar } from "@heroui/react";
-import Link from "next/link";
+import Sidebar from "@/components/sidebar/page";
 
 export default function AcheivementsPage() {
   return (
     <div className="flex h-screen bg-slate-100 text-gray-800">
-      {/* Sidebar */}
-      <aside className="w-60 bg-slate-200 p-4 flex flex-col gap-4">
-      <Link href="/" className="px-2 py-2 hover:bg-slate-300 rounded">DASHBOARD</Link>
-      <Link href="/tasks" className="px-2 py-2 hover:bg-slate-300 rounded">TASKS</Link>
-      <Link href="/leaderboard" className="px-2 py-2 hover:bg-slate-300 rounded">LEADERBOARD</Link>
-      <Link href="/achievements" className="px-2 py-2 hover:bg-slate-300 rounded">ACHIEVEMENTS</Link>
-      <Link href="/profile" className="px-2 py-2 hover:bg-slate-300 rounded">PROFILE</Link>
-    </aside>
+      <Sidebar />
 
-    {/* Main Content */}
-    <main className="flex-1 p-6 flex flex-col gap-6">
-      <div className="flex gap-6">
-        {/* Avatar + Achievements */}
-      </div>
-    </main>
-  </div>
+      <main className="flex-1 p-6 flex flex-col gap-6">
+        <div className="flex gap-6">
+        </div>
+      </main>
+    </div>
   );
 }
--- task-manager-starter/src/app/page.tsx
+++ task-manager-starter/src/app/page.tsx
@@ -3,7 +3,7 @@
 import { Progress } from "@heroui/react";
 import { Card, CardBody } from "@heroui/react";
 import { Avatar } from "@heroui/react";
-import Link from "next/link";
+import Sidebar from "@/components/sidebar/page";
 
 const tasks = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"];
 const achievements = ["Achievement 1", "Achievement 2", "Achievement 3"];
@@ -11,14 +11,7 @@ const achievements = ["Achievement 1", "Achievement 2", "Achievement 3"];
 export default function DashboardPage() {
   return (
     <div className="flex h-screen bg-slate-100 text-gray-800">
-      {/* Sidebar */}
-      <aside className="w-60 bg-slate-200 p-4 flex flex-col gap-4">
-      <Link href="/" className="px-2 py-2 hover:bg-slate-300 rounded">DASHBOARD</Link>
-      <Link href="/tasks" className="px-2 py-2 hover:bg-slate-300 rounded">TASKS</Link>
-      <Link href="/leaderboard" className="px-2 py-2 hover:bg-slate-300 rounded">LEADERBOARD</Link>
-      <Link href="/achievements" className="px-2 py-2 hover:bg-slate-300 rounded">ACHIEVEMENTS</Link>
-      <Link href="/profile" className="px-2 py-2 hover:bg-slate-300 rounded">PROFILE</Link>
-    </aside>
+        <Sidebar />
 
       {/* Main Content */}
       <main className="flex-1 p-6 flex flex-col gap-6">
